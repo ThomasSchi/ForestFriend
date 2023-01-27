1d6a490657357d4240bbb8eec0545cf01df32bbc
amiracon <contichamira@gmail.com>
2023-01-27 13:01:30 +0100

    TipsTricks

--- Websitegroup/src/app/app.module.ts
+++ Websitegroup/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PflanzenComponent } from './pflanzen/pflanzen.component';
+import { TipsTricksComponent } from './tips-tricks/tips-tricks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
     HomepageComponent,
-    PflanzenComponent
+    PflanzenComponent,
+    TipsTricksComponent
   ],
   imports: [
     BrowserModule,
